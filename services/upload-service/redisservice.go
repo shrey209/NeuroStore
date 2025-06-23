1df6a5bd620d27e89f850bdf5d9f47466cad1bee
shrey209 <shreyashkumar2022@gmail.com>
2025-06-23 17:29:33 +0530

    adding redis db for sha lookups

---
+++ services/upload-service/redisservice.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type ChunkMeta struct {
+	Filename string `json:"filename"`
+	Start    int    `json:"start"`
+	End      int    `json:"end"`
+}
+
+var ctx = context.Background()
+
+func CheckSHAExistence(rdb *redis.Client, shas []string) (existing []string, missing []string, err error) {
+	values, err := rdb.MGet(ctx, shas...).Result()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	for i, val := range values {
+		if val != nil {
+			existing = append(existing, shas[i])
+		} else {
+			missing = append(missing, shas[i])
+		}
+	}
+
+	return existing, missing, nil
+}
+
+func StoreSHAMetadata(rdb *redis.Client, metaMap map[string]ChunkMeta) error {
+	// Convert to a flat slice for MSet: [key1, val1, key2, val2, ...]
+	kvPairs := make([]interface{}, 0, len(metaMap)*2)
+
+	for sha, meta := range metaMap {
+		jsonVal, err := json.Marshal(meta)
+		if err != nil {
+			return err
+		}
+		kvPairs = append(kvPairs, sha, jsonVal)
+	}
+
+	return rdb.MSet(ctx, kvPairs...).Err()
+}
